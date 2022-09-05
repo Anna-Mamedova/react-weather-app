d33ec09e7dd1ab0ebc62be1afd7ca4b0ba9cfe37
Anna-Mamedova <mamedi289@gmail.com>
2022-09-05 14:51:53 +0300

    added units convert and change some css

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,29 +1,22 @@
 import React from "react"
 import CurrentDate from "./CurrentDate"
 import WeatherIcon from "./WeatherIcon"
+import WeatherTemperature from "./WeatherTemperature"
 
 export default function WeatherInfo(props) {
     return (
         <div className="WeatherInfo">
                 <div className="row">
-                  <div className="col-4">
-                    <p>
+                    <div className="col-4">
                         <WeatherIcon code = {props.date.icon}/>
-                        {/* <img src={props.date.icon} alt={props.date.description} className="icon" /> */}
-                        <span className="temperature">{Math.round(props.date.temperature)}</span>
-                        <span className="units">ºC</span>
-                    </p>
-                    <p className="dayTemperature">Day 27ºC / Night 17ºC</p>
-                  </div>
-                  <div className="col-3 dayIndicators">
-                    {/* <p className="rain">Rain: 4%</p> */}
-                    <p className="humidity">Humidity: {props.date.humidity}%</p>
-                    <p className="wind">Wind: {Math.round(props.date.wind)}km/h</p>
-                  </div>
-                  <div className="col-5 cityNow">
-                    <p className="city">{props.date.city}</p>
-                    <p className="data"><CurrentDate date={props.date.date} /></p>
-                    <p className="typeSky">{props.date.description}</p>
+                        <WeatherTemperature celsius = {Math.round(props.date.temperature)} />
+                        <p className="humidity dayIndicators">Humidity: {props.date.humidity}%</p>
+                        <p className="wind dayIndicators">Wind: {Math.round(props.date.wind)}km/h</p>
+                    </div>
+                    <div className="col-8 cityNow">
+                        <p className="city">{props.date.city}</p>
+                        <p className="data"><CurrentDate date={props.date.date} /></p>
+                        <p className="typeSky">{props.date.description}</p>
                   </div>
                 </div>
               </div>
