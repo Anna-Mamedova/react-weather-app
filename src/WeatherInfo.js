88464af26d75864eb6a5fe2dfec869771ce90245
Anna-Mamedova <mamedi289@gmail.com>
2022-09-09 15:59:58 +0300

    change col size in WeatherInfo.js

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -8,13 +8,13 @@ export default function WeatherInfo(props) {
     return (
         <div className="WeatherInfo">
                 <div className="row">
-                    <div className="col-4">
+                    <div className="col-5">
                         <WeatherIcon code = {props.date.icon} description = {props.date.description}/>
                         <WeatherTemperature celsius = {Math.round(props.date.temperature)} />
                         <p className="humidity dayIndicators">Humidity: {props.date.humidity}%</p>
                         <p className="wind dayIndicators">Wind: {Math.round(props.date.wind)}km/h</p>
                     </div>
-                    <div className="col-8 cityNow">
+                    <div className="col-7 cityNow">
                         <p className="city">{props.date.city}</p>
                         <p className="data"><CurrentDate date={props.date.date} /></p>
                         <p className="typeSky">{props.date.description}</p>
